67aa9f750db9bd25ee93e938ed5c67d94a963162
alicanpazarcikli <102393246+alicanpazarcikli@users.noreply.github.com>
2022-08-08 21:33:11 -0400

    new changes

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Github branching test....");
         System.out.println("working on alican's copy..");
+        System.out.println("Just a random change...");
     }
 }
