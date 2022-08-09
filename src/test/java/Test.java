2813c3bfaa44a00d2050371c54932e528d78ddd0
alicanpazarcikli <102393246+alicanpazarcikli@users.noreply.github.com>
2022-08-08 21:19:20 -0400

    alican's branch updates..

---
+++ src/test/java/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Github branching test....");
+        System.out.println("working on alican's copy..");
+    }
+}
