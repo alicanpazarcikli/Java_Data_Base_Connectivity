aa8c18f3e9b2ef84abd96fb202db84315744abe5
alicanpazarcikli <102393246+alicanpazarcikli@users.noreply.github.com>
2022-08-08 21:48:29 -0400

    an update

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -3,5 +3,6 @@ public class Test {
         System.out.println("Github branching test....");
         System.out.println("working on alican's copy..");
         System.out.println("Just a random change...");
+        System.out.println("last update...");
     }
 }
